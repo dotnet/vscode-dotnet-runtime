3056f047b1b243ed388e2a7a4646fff21257c1c9
N. Taylor Mullen <nimullen@microsoft.com>
2019-05-07 14:47:28 -0700

    Add progress indicators to the output channel.

--- src/OutputChannelObserver.ts
+++ src/OutputChannelObserver.ts
@@ -4,31 +4,48 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
-import { DotnetAcquisitionCompleted, DotnetAcquisitionError } from './EventStreamEvents';
+import { DotnetAcquisitionCompleted, DotnetAcquisitionError, DotnetAcquisitionStarted } from './EventStreamEvents';
 import { EventType } from './EventType';
 import { IEvent } from './IEvent';
 import { IEventStreamObserver } from './IEventStreamObserver';
 
 export class OutputChannelObserver implements IEventStreamObserver {
+    private downloadProgressInterval: NodeJS.Timeout | undefined;
     constructor(private readonly outputChannel: vscode.OutputChannel) {
     }
 
     public post(event: IEvent): void {
         switch (event.type) {
             case EventType.DotnetAcquisitionStart:
-                this.outputChannel.appendLine('Downloading .NET Core tooling...');
-                this.outputChannel.appendLine('');
+                const acquisitionStarted = event as DotnetAcquisitionStarted;
+                this.outputChannel.append(`Downloading .NET Core tooling '${acquisitionStarted.version}'...`);
+                this.startDownloadIndicator();
                 break;
             case EventType.DotnetAcquisitionCompleted:
                 const acquisitionCompleted = event as DotnetAcquisitionCompleted;
-                this.outputChannel.appendLine('.NET Core tooling installed!');
-                this.outputChannel.appendLine(`dotnet executable path: ${acquisitionCompleted.dotnetPath}`);
+                this.stopDownladIndicator();
+                this.outputChannel.appendLine(' Done!');
+                this.outputChannel.appendLine(`.NET Core executable path: ${acquisitionCompleted.dotnetPath}`);
+                this.outputChannel.appendLine('');
                 break;
             case EventType.DotnetAcquisitionError:
                 const error = event as DotnetAcquisitionError;
-                this.outputChannel.appendLine('Error occurred when downloing .NET Core tooling:');
+                this.stopDownladIndicator();
+                this.outputChannel.appendLine(' Error!');
+                this.outputChannel.appendLine('Failed to download .NET Core tooling:');
                 this.outputChannel.appendLine(error.getErrorMessage());
                 break;
+        }
+    }
+
+    private startDownloadIndicator() {
+        this.downloadProgressInterval = setInterval(() => this.outputChannel.append('.'), 1000);
+    }
+
+    private stopDownladIndicator() {
+        if (this.downloadProgressInterval) {
+            clearTimeout(this.downloadProgressInterval);
+            this.downloadProgressInterval = undefined;
         }
     }
 }
