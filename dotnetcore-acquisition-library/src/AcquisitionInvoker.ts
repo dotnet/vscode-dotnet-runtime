16315e410d228adc7813c1bd3d22799219beb82d
sfoslund <sfoslund@microsoft.com>
2019-11-08 14:48:25 -0800

    Updating License info

--- dotnetcore-acquisition-library/src/AcquisitionInvoker.ts
+++ dotnetcore-acquisition-library/src/AcquisitionInvoker.ts
@@ -1,3 +1,8 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
 import * as cp from 'child_process';
 import * as os from 'os';
 import * as path from 'path';
