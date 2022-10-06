0601849852b6fc6c3fbc412ecc5a90fe5e3e3ee5
Noah Gilson <noahgilson@microsoft.com>
2022-10-06 11:09:48 -0700

    Fetch branches beforehand

--- dependency-verifier.py
+++ dependency-verifier.py
@@ -2,7 +2,7 @@ import sys
 import subprocess
 
 targetBranch = sys.argv[1]
-
+result = subprocess.getoutput(f"git fetch --all")
 result = subprocess.getoutput(f"git diff --name-only {targetBranch}..")
 sys.exit(result)
 sys.exit(f"The yarn.lock and package-lock appear to be out of sync on {targetBranch}. Update by doing yarn import or yarn add for the following dependencies.")
