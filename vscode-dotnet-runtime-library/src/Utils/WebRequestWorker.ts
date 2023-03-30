a78686d50ec7957bb79f007e9e49555d20cb38d7
Noah Gilson <noahgilson@microsoft.com>
2023-03-30 11:53:18 -0700

    Account for bad urls in axios get

--- vscode-dotnet-runtime-library/src/Utils/WebRequestWorker.ts
+++ vscode-dotnet-runtime-library/src/Utils/WebRequestWorker.ts
@@ -79,6 +79,10 @@ export class WebRequestWorker {
      */
     private async axiosGet(url : string, options = {})
     {
+        if(url === '' || url === undefined || url === null)
+        {
+            throw new Error(`Request to the url ${this.url} failed, as the URL is invalid.`);
+        }
         const abort = axios.CancelToken.source()
         const id = setTimeout(
             () => abort.cancel(`Timeout, ${url} is unavailable.`),
