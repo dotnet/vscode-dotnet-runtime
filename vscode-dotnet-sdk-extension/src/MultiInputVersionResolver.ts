802f05c877c938e4f2adae627ac9f5642ba03833
Noah Gilson <noahgilson@microsoft.com>
2023-04-10 16:58:06 -0700

    Add basic code for Global Installer Resolver to determine what version to install.
    This file is not complete yet. This set of changes includes changes to determine
    what version the user requested. Some of this code will be put into the VersionResolver later.
    This global installer resolver (currently named multi input version resolver) is what finds the correct installer to download.

--- vscode-dotnet-sdk-extension/src/MultiInputVersionResolver.ts
+++ vscode-dotnet-sdk-extension/src/MultiInputVersionResolver.ts
@@ -4,8 +4,9 @@ import { IEventStream } from 'vscode-dotnet-runtime-library'
 import { IExtensionState } from 'vscode-dotnet-runtime-library';
 import { DotnetVersionResolutionError } from 'vscode-dotnet-runtime-library';
 import { DotnetVersionResolutionCompleted } from 'vscode-dotnet-runtime-library';
+import * as os from 'os';
 
-export class MultiInputVersionResolver implements IVersionResolver {
+export class GlobalSDKInstallerUrlResolver {
     /**
      * @remarks
      * This is similar to the version resolver but accepts a wider range of inputs such as '6', '6.1', or '6.0.3xx' or '6.0.301'.
@@ -20,23 +21,108 @@ export class MultiInputVersionResolver implements IVersionResolver {
         this.webWorker = new WebRequestWorker(extensionState, eventStream, this.releasesUrl, this.releasesKey);
     }
 
-    public async getFullRuntimeVersion(version: string): Promise<string> {
-            throw new Error("Not Implemented: Getting the Runtime Version from this format of Versions is not implemented."); 
+    public async getCorrectInstallerUrl(version: string): Promise<string> {
+        return this.routeRequestToProperVersionRequestType(version);
     }
 
-    public async getFullSDKVersion(version: string): Promise<string> {
-        return this.getFullVersion(version);
-    }
+    private async routeRequestToProperVersionRequestType(version : string) : Promise<string> {
+        /**
+         * @remarks TODO: Do we need to support -preview or things with build numbers inside?
+         */
 
-    private async getFullVersion(version: string): Promise<string> {
-        try {
-            //const releasesVersions = await this.getReleasesInfo();
-            //const versionResult = this.resolveVersion(version, releasesVersions);
-            //this.eventStream.post(new DotnetVersionResolutionCompleted(version, versionResult));
-            return "";
-        } catch (error) {
-            this.eventStream.post(new DotnetVersionResolutionError(error as Error, version));
-            throw error;
+        let numberOfPeriods = version.split('.').length - 1;
+
+        // TODO: turn these if conditions into functions with names
+        if(this.isNumber(version) && numberOfPeriods == 0)
+        {
+            // A major release represented as an integer was given. e.g. 6
+            // call into releases json and get latest-sdk 
+            // call into getinstallerurl
         }
+        else if(this.isNumber(version) && numberOfPeriods == 1)
+        {
+            // A major.minor was given e.g. 6.1
+            // call into releases json and get latest-sdk
+            // call into get installer url
+        }
+        else if(version.split(".").slice(0, 2).every(x => this.isNumber(x)) && version.endsWith('x') && numberOfPeriods == 2 && version.length < 9) // 9 is used to prevent bad versions (current expectation is 7 but we want to support .net 10 etc)
+        {
+            // A feature band was given e.g. 6.0.4xx or 6.0.40x
+        }
+        else if(version.split(".").every(x => this.isNumber(x)) && numberOfPeriods == 2 && version.length < 9)
+        {
+            // A specific version e.g. 7.0.301
+            // call into get installer url
+        }
+        throw Error(`The version requested: ${version} is not in a valid format.`)
+    }
+
+    private getInstallerUrl(specificVersion : string) : string
+    {
+        let operatingSys : string = os.platform();
+        let operatingArch : string = os.arch();
+
+        let convertedOs = "";
+        let convertedArch = "";
+        
+        switch(operatingSys)
+        {
+            case 'win32': {
+                convertedOs = 'win';
+                break;
+            }
+            case 'darwin': {
+                convertedOs = 'osx';
+                break;
+            }
+            case 'linux': {
+                convertedOs = operatingSys;
+                break;
+            }
+        }
+
+        switch(operatingArch)
+        {
+            case 'x64': {
+                convertedArch = operatingArch;
+                break;
+            }
+            case 'x32': {
+                convertedArch = 'x86';
+                break;
+            }
+            case 'arm': {
+                convertedArch = operatingArch;
+                break;
+            }
+            case 'arm64': {
+                convertedArch = operatingArch;
+                break;
+            }
+        }
+
+        var desiredRidPackage = convertedOs + '-' + convertedArch;
+        // call get major function
+        // get releases url
+        // get releases.sdks.sdk where sdk.version == fullversion
+        // get files 
+        // from files get x where x.rid is desiredRidPackage
+        // if none found, fail
+
+        return "";
+    }
+
+    private createIndexUrl(majorMinor : string ): string
+    {
+        return 'https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/' + majorMinor + '/releases.json';
+    }
+
+    private isNumber(value: string | number): boolean
+    {
+        return (
+            (value != null) &&
+            (value !== '') &&
+            !isNaN(Number(value.toString()))
+        );
     }
 }
