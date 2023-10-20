1d0ed453e6d90b0ea465b5e552b8e0a65456968a
Noah Gilson <noahgilson@microsoft.com>
2023-10-20 10:09:18 -0700

    fix linting issue

--- vscode-dotnet-runtime-library/src/Acquisition/WinMacGlobalInstaller.ts
+++ vscode-dotnet-runtime-library/src/Acquisition/WinMacGlobalInstaller.ts
@@ -7,6 +7,7 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 import * as vscode from 'vscode';
+/* tslint:disable:no-any */
 
 import { FileUtilities } from '../Utils/FileUtilities';
 import { IGlobalInstaller } from './IGlobalInstaller';
