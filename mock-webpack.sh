d0f62ad0d0137d503d21a77de0e7ed67577bc58f
Noah Gilson <noahgilson@microsoft.com>
2024-01-19 16:49:38 -0800

    webpack install scripts for library code

--- mock-webpack.sh
+++ mock-webpack.sh
@@ -2,3 +2,4 @@ echo ""
 echo "----------- Copying Library Webpacked Dependencies -----------"
 echo "" # See the build.ps1 for more details on why we do this
 cp -r ./vscode-dotnet-runtime-library/distro-data ./vscode-dotnet-runtime-library/dist/Acquisition
+cp -r ./vscode-dotnet-runtime-library/install-scripts ./vscode-dotnet-runtime-library/dist/utils
