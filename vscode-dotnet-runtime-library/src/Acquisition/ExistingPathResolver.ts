3682e819298cc2058adc6a9f4a7bbc6012e24a43
annie <59816815+JL03-Yue@users.noreply.github.com>
2024-04-01 13:51:24 -0700

    update ExistingPathResolver

--- vscode-dotnet-runtime-library/src/Acquisition/ExistingPathResolver.ts
+++ vscode-dotnet-runtime-library/src/Acquisition/ExistingPathResolver.ts
@@ -8,23 +8,6 @@ import { IDotnetAcquireResult } from '../IDotnetAcquireResult';
 import { IExistingPaths, ILocalExistingPath } from '../IExtensionContext';
 
 export class ExistingPathResolver {
-    // public resolveExistingPath(existingPaths: ILocalExistingPath[] | undefined, extensionId: string | undefined, windowDisplayWorker: IWindowDisplayWorker): IDotnetAcquireResult | undefined {
-    //     if (existingPaths) {
-    //         if (!extensionId) {
-    //             // Use the global path if it is specified
-    //             if (existingPaths)
-    //             windowDisplayWorker.showWarningMessage(
-    //                 'Ignoring existing .NET paths defined in settings.json because requesting extension does not define its extension ID. Please file a bug against the requesting extension.',
-    //                 () => { /* No callback */ },
-    //             );
-    //             return;
-    //         }
-    //         const existingPath = existingPaths.filter((pair) => pair.extensionId === extensionId);
-    //         if (existingPath && existingPath.length > 0) {
-    //             return { dotnetPath: existingPath![0].path };
-    //         }
-    //     }
-    // }
     public resolveExistingPath(existingPaths: IExistingPaths | undefined, extensionId: string | undefined, windowDisplayWorker: IWindowDisplayWorker): IDotnetAcquireResult | undefined {
         if (existingPaths) {
             if (!extensionId) {
