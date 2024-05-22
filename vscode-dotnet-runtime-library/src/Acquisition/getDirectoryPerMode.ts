c2d71fa3c72b48fa95866361662a3854297ab055
Noah Gilson <noahgilson@microsoft.com>
2024-05-22 16:11:58 -0700

    add missing file header from refactor

--- vscode-dotnet-runtime-library/src/Acquisition/getDirectoryPerMode.ts
+++ vscode-dotnet-runtime-library/src/Acquisition/getDirectoryPerMode.ts
@@ -1,6 +1,12 @@
-import { DotnetInstallMode } from "./DotnetInstallMode";
-import { RuntimeInstallationDirectoryProvider } from "./RuntimeInstallationDirectoryProvider";
-import { SdkInstallationDirectoryProvider } from "./SdkInstallationDirectoryProvider";
+/* --------------------------------------------------------------------------------------------
+ *  Licensed to the .NET Foundation under one or more agreements.
+*  The .NET Foundation licenses this file to you under the MIT license.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import { DotnetInstallMode } from './DotnetInstallMode';
+import { RuntimeInstallationDirectoryProvider } from './RuntimeInstallationDirectoryProvider';
+import { SdkInstallationDirectoryProvider } from './SdkInstallationDirectoryProvider';
 
 
 export function getDirectoryPerMode(mode: DotnetInstallMode, storagePath: string) {
