bba9719f4b57d4bfc7c67815416ec1c7ef1f6b60
Noah Gilson <nagilson@umich.edu>
2024-07-16 15:40:52 -0700

    Interpret Installer Exit Codes (#1880)
    * Interpret Installer Exit Codes
    
    These will give the user more actionable items, and also increase the chances the post a log so we can help them if something goes wrong and understand the issue better.
    
    * Add page fault message
    
    * Add Timeout error code
    
    * Use Native Code Message
    
    * Use err message from https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--1300-1699-
    
    * Use particular error code from windows
    
    * Use the official error message.

--- vscode-dotnet-runtime-library/src/Acquisition/WinMacGlobalInstaller.ts
+++ vscode-dotnet-runtime-library/src/Acquisition/WinMacGlobalInstaller.ts
@@ -79,6 +79,42 @@ export class WinMacGlobalInstaller extends IGlobalInstaller {
         this.webWorker = new WebRequestWorker(context, installerUrl);
     }
 
+    public static InterpretExitCode(code : string) : string
+    {
+        const reportLogMessage = `Please provide your .NET Installer log (note our privacy notice), which can be found at %temp%.
+The file has a name like 'Microsoft_.NET_SDK*.log and should appear in recent files.
+This report should be made at https://github.com/dotnet/vscode-dotnet-runtime/issues.`
+
+        switch(code)
+        {
+            case '1':
+                return `The .NET SDK installer has failed with a generic failure. ${reportLogMessage}`;
+            case '5':
+                return `Insufficient permissions are available to install .NET. Please run the installer as an administrator.`;
+            case '67':
+                return `The network name cannot be found. ${reportLogMessage}`;
+            case '112':
+                return `The disk is full. Please free up space and try again.`;
+            case '255':
+                return `The .NET Installer was terminated by another process unexpectedly. Please try again.`;
+            case '1260':
+                return `The .NET SDK is blocked by group policy. Can you please report this at https://github.com/dotnet/vscode-dotnet-runtime/issues`
+            case '1460':
+                return `The .NET SDK had a timeout error. ${reportLogMessage}`;
+            case '1603':
+                return `Fatal error during .NET SDK installation. ${reportLogMessage}`;
+            case '1618':
+                return `Another installation is already in progress. Complete that installation before proceeding with this install.`;
+            case '000751':
+                return `Page fault was satisfied by reading from a secondary storage device. ${reportLogMessage}`;
+            case '2147500037':
+                return `An unspecified error occurred. ${reportLogMessage}`;
+            case '2147942405':
+                return `Insufficient permissions are available to install .NET. Please try again as an administrator.`;
+        }
+        return '';
+    }
+
     public async installSDK(install : DotnetInstall): Promise<string>
     {
         // Check for conflicting windows installs
