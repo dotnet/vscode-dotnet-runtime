38edf5482f3136dd3b5d89dd89f2ec215d5cb117
Noah Gilson <noahgilson@microsoft.com>
2024-10-07 13:18:58 -0700

    Fix the parsing of other regional characters for find path API (#1969)
    * Use Unicode Code Page to Parse dotnet output
    
     For Windows, we need to change the code page to UTF-8 to handle the output of the command. https://github.com/nodejs/node-v0.x-archive/issues/2190
    
    Resolves https://github.com/dotnet/vscode-dotnet-runtime/issues/1967
    
    * Ensure that utf8 is available

--- vscode-dotnet-runtime-library/src/Acquisition/DotnetConditionValidator.ts
+++ vscode-dotnet-runtime-library/src/Acquisition/DotnetConditionValidator.ts
@@ -11,9 +11,8 @@ import { IDotnetListInfo } from './IDotnetListInfo';
 import { IAcquisitionWorkerContext } from './IAcquisitionWorkerContext';
 import { IDotnetConditionValidator } from './IDotnetConditionValidator';
 import * as versionUtils from './VersionUtilities';
+import * as os from 'os';
 import { FileUtilities } from '../Utils/FileUtilities';
-import { EnvironmentVariableIsDefined } from '../Utils/TypescriptUtilities';
-
 
 export class DotnetConditionValidator implements IDotnetConditionValidator
 {
@@ -76,7 +75,8 @@ export class DotnetConditionValidator implements IDotnetConditionValidator
 
     public async getSDKs(existingPath : string) : Promise<IDotnetListInfo[]>
     {
-        const findSDKsCommand = CommandExecutor.makeCommand(`"${existingPath}"`, ['--list-sdks']);
+        const findSDKsCommand = await this.setCodePage() ? CommandExecutor.makeCommand(`chcp`, [`65001`, `|`,`"${existingPath}"`, '--list-sdks']) :
+            CommandExecutor.makeCommand(`"${existingPath}"`, ['--list-sdks']);
 
         const sdkInfo = await (this.executor!).execute(findSDKsCommand, undefined, false).then((result) =>
         {
@@ -95,6 +95,13 @@ export class DotnetConditionValidator implements IDotnetConditionValidator
         });
 
         return sdkInfo;
+    }
+
+    private async setCodePage() : Promise<boolean>
+    {
+        // For Windows, we need to change the code page to UTF-8 to handle the output of the command. https://github.com/nodejs/node-v0.x-archive/issues/2190
+        // Only certain builds of windows support UTF 8 so we need to check that we can use it.
+        return os.platform() === 'win32' ? (await this.executor!.tryFindWorkingCommand([CommandExecutor.makeCommand('chcp', ['65001'])])) !== null : false;
     }
 
     private stringVersionMeetsRequirement(foundVersion : string, requiredVersion : string, requirement : DotnetVersionSpecRequirement) : boolean
@@ -122,7 +129,8 @@ export class DotnetConditionValidator implements IDotnetConditionValidator
 
     public async getRuntimes(existingPath : string) : Promise<IDotnetListInfo[]>
     {
-        const findRuntimesCommand = CommandExecutor.makeCommand(`"${existingPath}"`, ['--list-runtimes']);
+        const findRuntimesCommand = await this.setCodePage() ? CommandExecutor.makeCommand(`chcp`, [`65001`, `|`,`"${existingPath}"`, '--list-runtimes']) :
+            CommandExecutor.makeCommand(`"${existingPath}"`, ['--list-runtimes']);
 
         const windowsDesktopString = 'Microsoft.WindowsDesktop.App';
         const aspnetCoreString = 'Microsoft.AspNetCore.App';
