53457f8fc9608c874516c2fbc8aec0ae7f61352f
Noah Gilson <noahgilson@microsoft.com>
2025-01-08 09:41:08 -0800

    transform the response after its done with the get function
    this is so the json serialization is not in the timer.

--- vscode-dotnet-runtime-library/src/Utils/WebRequestWorker.ts
+++ vscode-dotnet-runtime-library/src/Utils/WebRequestWorker.ts
@@ -41,6 +41,7 @@ import {
     WebRequestTime
 } from '../EventStream/EventStreamEvents';
 import { getInstallFromContext } from './InstallIdUtilities';
+import { json } from 'stream/consumers';
 
 export class WebRequestWorker
 {
@@ -299,9 +300,22 @@ export class WebRequestWorker
             this.context.eventStream.post(new WebRequestSent(this.url));
             const response = await this.axiosGet(
                 this.url,
-                options
+                {transformResponse: (x : any) => x, ... options}
             );
 
+            if(response !== null && response.headers['content-type'] === 'application/json')
+            {
+                try
+                {
+                    // Try to copy logic from https://github.com/axios/axios/blob/2e58825bc7773247ca5d8c2cae2ee041d38a0bb5/lib/defaults/index.js#L100
+                    const jsonData = JSON.parse(response.data);
+                    return jsonData;
+                }
+                catch(error : any)
+                {
+
+                }
+            }
             return response.data;
         }
         catch (error : any)
