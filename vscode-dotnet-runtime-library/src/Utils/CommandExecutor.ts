fe8c2de5236a4d84d91ea632d996c23d6a0ea050
Noah Gilson <noahgilson@microsoft.com>
2025-04-09 15:11:10 -0700

    Fix lint

--- vscode-dotnet-runtime-library/src/Utils/CommandExecutor.ts
+++ vscode-dotnet-runtime-library/src/Utils/CommandExecutor.ts
@@ -98,6 +98,7 @@ export class CommandExecutor extends ICommandExecutor
         }
         catch (error: any)
         {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
             error.message = error.message + `\nFailed to create ${this.sudoProcessCommunicationDir}. Please check your permissions or install dotnet manually.`;
             this.context?.eventStream.post(new SudoDirCreationFailed(`The command ${fullCommandString} failed, as no directory could be made: ${JSON.stringify(error)}`));
         }
