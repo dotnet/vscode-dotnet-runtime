44fda482e0c8515c99e0c56291a2c2e79a88523a
Noah Gilson <noahgilson@microsoft.com>
2025-07-08 13:50:48 -0700

    Fix Mock and Utils copy
    the expectatoin is not in the utils folder, idk why its in there. make sure this does not impact the vsix behavior for sudo but that will be caught in vendor testing.

--- mock-webpack.ps1
+++ mock-webpack.ps1
@@ -1,4 +1,4 @@
 # The library doesn't get webpacked, but it needs the copy of items that would normally be webpacked
 # ... into the SDK or Runtime Extension for it to run in local dev scenarios.
 Copy-Item ".\vscode-dotnet-runtime-library\distro-data\" -Destination ".\vscode-dotnet-runtime-library\dist\Acquisition\" -Recurse -Force
-Copy-Item ".\vscode-dotnet-runtime-library\install scripts\" -Destination ".\vscode-dotnet-runtime-library\dist\utils\" -Recurse -Force
+Copy-Item ".\vscode-dotnet-runtime-library\install scripts\" -Destination ".\vscode-dotnet-runtime-library\dist\" -Recurse -Force
