794958913cbf4415c031e1515a9ec5423568cf1d
Noah Gilson <noahgilson@microsoft.com>
2025-10-15 11:29:03 -0700

    Remove extra calls to install is in use
    This happens already in the track installed version code.

--- vscode-dotnet-runtime-library/src/Acquisition/DotnetCoreAcquisitionWorker.ts
+++ vscode-dotnet-runtime-library/src/Acquisition/DotnetCoreAcquisitionWorker.ts
@@ -173,7 +173,6 @@ export class DotnetCoreAcquisitionWorker implements IDotnetCoreAcquisitionWorker
                         possibleInstallWithSameMajorMinor.version));
                     context.eventStream.post(new DotnetOfflineInstallUsed(`We detected you are offline and are using the pre-existing .NET installation ${install.dotnetInstall.installId}.
 To keep your .NET version up to date, please reconnect to the internet at your soonest convenience.`))
-                    await InstallTrackerSingleton.getInstance(context.eventStream, context.extensionState).markInstallAsInUse(dotnetExePath);
                     await InstallTrackerSingleton.getInstance(context.eventStream, context.extensionState).trackInstalledVersion(context, install.dotnetInstall, dotnetExePath);
                     return { dotnetPath: dotnetExePath };
                 }
@@ -354,7 +353,6 @@ To keep your .NET version up to date, please reconnect to the internet at your s
                     ? context.acquisitionContext.requestingExtensionId : null));
 
             await InstallTrackerSingleton.getInstance(context.eventStream, context.extensionState).trackInstalledVersion(context, install, dotnetPath);
-            await InstallTrackerSingleton.getInstance(context.eventStream, context.extensionState).markInstallAsInUse(dotnetPath);
             return dotnetPath;
         }
         return null;
